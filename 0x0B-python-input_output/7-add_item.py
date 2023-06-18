cbf75a20f45e9a8565807c1007158ca0a9dd8d09
ugoMusk <ugobothankgod@gmail.com>
2023-06-18 16:45:35 +0100

    adds all args to a list and save as json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-
-"""This module adds all arguments to a list and save them to a file.
-"""
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
     try:
