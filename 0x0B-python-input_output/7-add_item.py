0ba754a2eabc7cecc20ff412277f4d91e9caad58
ugoMusk <ugobothankgod@gmail.com>
2023-06-18 16:40:54 +0100

    adds all args to a list and save as json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""This module adds all cl args to a list and save them to a file.
-"""
 
+"""This module adds all arguments to a list and save them to a file.
+"""
 
 import sys
 
@@ -14,5 +14,6 @@ if __name__ == "__main__":
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
+        
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
