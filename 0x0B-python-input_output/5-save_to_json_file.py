e0f99546e37635dedd5cc761bb89a4424e2fac7a
ugoMusk <ugobothankgod@gmail.com>
2023-06-18 16:04:44 +0100

    function that converts and writes a python object to a file in json format

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module contains  a function that converts and writes a python object to a file in JSON format"""
+"""This module contains object_to_json fuction"""
 import json
 
 
