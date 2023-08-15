9194a4abe4f54b4d3810dc90f61eaa1d7adb6f60
ugoMusk <ugobothankgod@gmail.com>
2023-08-15 01:40:29 +0100

    make 8-square executable

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
-'use strict';
-let x = process.argv[2];
-if (isNaN(x)) {
+const x = process.argv[2];
+
+if (!parseInt(x)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < x; i++) {
-    console.log('X'.repeat(x));
+    let y = 0;
+    let myVar = '';
+
+    while (y < x) {
+      myVar = myVar + 'X';
+      y++;
+    }
+    console.log(myVar);
   }
 }
